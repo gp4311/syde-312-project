7c8779703172f4ad7dd4acc34832b178b792f2cc
sophiahutch <sophia2.hutchison@gmail.com>
2026-03-16 14:34:28 -0400

    sped up inverse matrix video

--- inverse_matrix.py
+++ inverse_matrix.py
@@ -21,7 +21,7 @@ class InverseMatrix(Scene):
     def construct(self):
         self.title_label = Text("Finding the Inverse of the Key Matrix", font_size=20).to_edge(UP)
         self.play(Write(self.title_label))
-        self.wait(0.5)
+        self.wait(0.1)
 
         self.current_stage = self.create_stage(
              to_latex_matrix([
@@ -31,12 +31,12 @@ class InverseMatrix(Scene):
                 ])
         )
         self.play(Create(self.current_stage))
-        self.wait()
+        self.wait(.1)
         self.key_label = Text("Key         Identity", font_size=24)
         self.key_label.next_to(self.current_stage, DOWN)
         
         self.play(Write(self.key_label))
-        self.wait()
+        self.wait(.1)
 
         self.play(FadeOut(self.title_label), FadeOut(self.key_label))
 
@@ -47,7 +47,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_1 \rightarrow R_1 - 9R_3")
 
-        self.wait(.5)
+        self.wait(.1)
         
         self.rows(
             [[0, -154, -206 , 1, 0, -9],
@@ -56,7 +56,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_3 \rightarrow R_3 - \frac{1}{2} R_2")
 
-        self.wait(.5)
+        self.wait(.1)
         self.rows(
             [[0, -154, -206 , 1, 0, -9],
                 [4, 0, Fraction(-20,17), 0, Fraction(19,17), Fraction(-4,17)],
@@ -64,7 +64,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_2 \rightarrow R_2 - \frac{4}{17} R_3")
 
-        self.wait(.5)
+        self.wait(.1)
         self.rows(
             [[0, -154, -206 , 1, 0, -9],
                 [1, 0, Fraction(-5,17), 0, Fraction(19,68), Fraction(-1,17)],
@@ -72,7 +72,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_2 / 4")
 
-        self.wait(.5)
+        self.wait(.1)
         self.rows(
             [[0, 0, Fraction(-114,17) , 1, Fraction(-77,17), Fraction(1,17)],
                 [1, 0, Fraction(-5,17), 0, Fraction(19,68), Fraction(-1,17)],
@@ -80,7 +80,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_1 \rightarrow R_1 + \frac{154}{17} R_3")
             
-        self.wait(.5)
+        self.wait(.1)
         self.rows(
             [[0, 0, 1 , Fraction(-17,114), Fraction(77,114), Fraction(-1,114)],
                 [1, 0, Fraction(-5,17), 0, Fraction(19,68), Fraction(-1,17)],
@@ -88,7 +88,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_1 \rightarrow R_1 \cdot \left(-\frac{17}{114}\right)")
 
-        self.wait(.5)
+        self.wait(.1)
         
         self.rows(
             [[0, 0, 1 , Fraction(-17,114), Fraction(77,114), Fraction(-1,114)],
@@ -97,7 +97,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_3 \rightarrow R_3 - 22R_1")
 
-        self.wait(.5)
+        self.wait(.1)
 
         self.rows(
             [[0, 0, 1 , Fraction(-17,114), Fraction(77,114), Fraction(-1,114)],
@@ -106,7 +106,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_3 / 17")
 
-        self.wait(.5)
+        self.wait(.1)
 
         self.rows(
             [[0, 0, 1 , Fraction(-17,114), Fraction(77,114), Fraction(-1,114)],
@@ -115,7 +115,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_2 \rightarrow R_2 + 5/17R_1")
 
-        self.wait(.5)
+        self.wait(.1)
 
         self.rows(
             [[1, 0, 0, Fraction(-5,114), Fraction(109,228), Fraction(-7,114)],
@@ -124,7 +124,7 @@ class InverseMatrix(Scene):
                 ],
             r"R_1 \leftrightarrow R_2")
 
-        self.wait(.5)
+        self.wait(.1)
         self.rows([[1, 0, 0, Fraction(-5,114), Fraction(109,228), Fraction(-7,114)],
                 [0, 1, 0, Fraction(11,57), Fraction(-103,114), Fraction(4,57)],
                 [0, 0, 1 , Fraction(-17,114), Fraction(77,114), Fraction(-1,114)]
@@ -132,11 +132,11 @@ class InverseMatrix(Scene):
             r"R_2 \leftrightarrow R_3")
         
 
-        self.wait(1)
+        self.wait(.1)
         final_label = Text("Identity     Key Inverse", font_size=24)
         final_label.next_to(self.current_stage, DOWN, buff=0.5)
         self.play(Write(final_label))
-        self.wait(2)
+        self.wait(.1)
 
     def create_stage(self, matrix):
         key_matrix = Matrix(
@@ -170,4 +170,4 @@ class InverseMatrix(Scene):
 
         self.op = row_operations
         
-        self.wait()
+        self.wait(.1)
